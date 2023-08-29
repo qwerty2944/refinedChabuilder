94ca93eebe91962f419cdf6acb0ac8dd1ccfab34
qwerty2944 <qwerty.2944@gmail.com>
2023-08-30 00:13:22 +0900

    냠냠굿

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation';
 export default function Home() {
   return (
     <main>
-      <h1>왜안되 씨발</h1>
+      <h1>냠냠굿</h1>
       <Chabuilder />
     </main>
   );
--- src/components/chabuilder.tsx
+++ src/components/chabuilder.tsx
@@ -6,7 +6,15 @@ import React, { useEffect, useState, useImperativeHandle } from 'react';
 export default function Chabuilder() {
   // const selectList = ['apple', 'banana', 'grape', 'orange'];
   const [Selected, setSelected] = useState('');
+  // 색상 상태를 관리하기 위한 state
+  const [selectedColor, setSelectedColor] = useState('#ffffff');
 
+  // 색상이 변경될 때 실행될 핸들러
+  const handleColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newColor = e.target.value;
+    setSelectedColor(newColor);
+    changeHairColor(newColor);
+  };
   // const handleSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
   //   setSelected(e.target.value);
   // };
@@ -19,18 +27,37 @@ export default function Chabuilder() {
       codeUrl: '/chabuilder/Build/chabuilder.wasm',
     });
 
-  function ChangeSpecies(index?: number) {
+  function changeSpecies(index?: number) {
     if (index == undefined) {
       index = Math.floor(Math.random() * 22);
     }
-    console.log('냠냠굿');
     sendMessage('UnitRoot', 'changeSpecies', index);
+  }
+  function changeHair(index?: number) {
+    if (index == undefined) {
+      index = Math.floor(Math.random() * 62);
+    }
+
+    sendMessage('UnitRoot', 'changeHair', index);
+  }
+  function changeHairColor(hex: String) {
+    sendMessage('UnitRoot', 'changeHairColor', hex.toString());
   }
 
   return (
-    <div className='bg-red-100'>
-      <Unity unityProvider={unityProvider} />
-      <button onClick={() => ChangeSpecies()}>념념긋</button>
+    <div>
+      <div className='bg-red-100'>
+        <Unity unityProvider={unityProvider} />
+      </div>
+      <div className='flex flex-col items-start'>
+        <button onClick={() => changeSpecies()}>종족바꾸기</button>
+        <button onClick={() => changeHair()}>머리바꾸기</button>
+        <input
+          type='color'
+          value={selectedColor}
+          onChange={handleColorChange}
+        />
+      </div>
     </div>
   );
 }
